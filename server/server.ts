6137e9ea8f9157906027b687066d51e0bffb3a4c
Swarn <swarnchaudhary170@gmail.com>
2026-05-11 23:07:40 +0545

    chore:project completed

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import { toNodeHandler } from 'better-auth/node';
 import { auth } from './lib/auth.js';
 import userRouter from './routes/userRoutes.js';
 import projectRouter from './routes/projectRoutes.js';
+import { stripeWebhook } from './controllers/stripeWebhook.js';
 // import { toNodeHandler } from 'better-auth/node';
 // import { auth } from './lib/auth.js';
 // import userRouter from './routes/userRoutes.js';
@@ -24,6 +25,9 @@ app.use(cors(corsOptions));
 app.use(express.json({
     limit:"50mb"
 }));
+
+app.post('/api/stripee',  express.raw({ type: "application/json" }),
+  stripeWebhook)
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
